dc0693e821665a0494e79b52ac972b9c4f4c1e17
Zhou1213CN <48866521+Zhou1213CN@users.noreply.github.com>
2021-08-30 23:54:59 -0400

    Delete ProjectPage.js

--- ProjectPage.js
+++
@@ -1,9 +0,0 @@
-import  ProjectDetail  from './ProjectDetail.js';
-
-export default function ProjectPage(project) {
-  document.querySelector(".container").innerHTML = `
-    <a href=".">Go Back</a><br><br>
-    ${ProjectDetail(project)}
-    <footer>Copyright © 2020 Zheng Zhou </footer>
-  `;
-}
